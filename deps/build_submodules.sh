d26dec587b7cff2d3695367c748e55a2a29960cb
Alex Nelson <moustique.mordu@gmail.com>
2013-05-11 14:53:43 -0700

    Add submodule builder script that tried FUSE out

---
+++ deps/build_submodules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+
+#Build FUSE - Not actually necessary for upartsfs, and the automake-derived Make rules for fusexmp weren't terribly helpful in the module's examples/ directory.  The build also fails without sudo.
+exit 0    #Bail on building FUSE
+pushd ./fuse
+./makeconf.sh
+./configure --prefix=$PWD/build
+make
+make check
+make distcheck #This step happens to fail; $(DESTDIR) in the util/ Automake rules defaults to /sbin, and this test is purposefully run without sudo.
+make install 
+popd
