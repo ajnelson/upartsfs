62b9ac5c56efe48e636608ce33854bd7beb03fa2
Alex Nelson <moustique.mordu@gmail.com>
2013-05-11 15:15:42 -0700

    Add build steps for TSK

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+set -e
+set -x
+
+#Assure FUSE is checked out
 git submodule init deps/fuse
 git submodule update deps/fuse
+
+#Assure TSK is built
+if [ ! -r "deps/sleuthkit/build/include/libtsk.h" ]; then
+  echo "Note: Building TSK..." >&2
+  git submodule init deps/sleuthkit
+  git submodule update deps/sleuthkit
+  pushd deps
+  ./build_submodules.sh
+  popd
+fi
