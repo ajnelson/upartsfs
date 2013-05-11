ab9c27598e3aeed46baaf49089aed865e759c32c
Alex Nelson <moustique.mordu@gmail.com>
2013-05-11 15:16:33 -0700

    Add TSK build instructions

--- deps/build_submodules.sh
+++ deps/build_submodules.sh
@@ -3,6 +3,14 @@
 set -e
 set -x
 
+
+pushd sleuthkit
+./bootstrap
+./configure --prefix=$PWD/build
+make -j
+make install
+popd
+
 #Build FUSE - Not actually necessary for upartsfs, and the automake-derived Make rules for fusexmp weren't terribly helpful in the module's examples/ directory.  The build also fails without sudo.
 exit 0    #Bail on building FUSE
 pushd ./fuse
