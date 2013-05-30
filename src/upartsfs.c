784d28c607544f6bb327a8a939d323fc4e2f7f2b
Alex Nelson <moustique.mordu@gmail.com>
2013-05-29 21:30:51 -0400

    Replace xmp write with squawky nop

--- src/upartsfs.c
+++ src/upartsfs.c
@@ -260,23 +260,10 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 	return res;
 }
 
-static int xmp_write(const char *path, const char *buf, size_t size,
+static int uparts_write(const char *path, const char *buf, size_t size,
 		     off_t offset, struct fuse_file_info *fi)
 {
-	int fd;
-	int res;
-
-	(void) fi;
-	fd = open(path, O_WRONLY);
-	if (fd == -1)
-		return -errno;
-
-	res = pwrite(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
-
-	close(fd);
-	return res;
+	return -EOPNOTSUPP;
 }
 
 static int xmp_statfs(const char *path, struct statvfs *stbuf)
@@ -392,7 +379,7 @@ static struct fuse_operations upartsfs_oper = {
 #endif
 	.open		= xmp_open,
 	.read		= xmp_read,
-	.write		= xmp_write,
+	.write		= uparts_write,
 	.statfs		= xmp_statfs,
 	.release	= xmp_release,
 	.fsync		= xmp_fsync,
