93c7f8ae6885e0e2e116af4d13abc1c4583f736e
Alex Nelson <ajnelson@acm.org>
2013-08-31 15:42:03 -0400

    test_open.c: Revise debug statements

--- tests/test_open.c
+++ tests/test_open.c
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
     char* file_path;
     char* mode_string;
     int file_descriptor;
-    int mode_flag;
+    mode_t mode_flag;
 
     if (argc < 3) {
         fprintf(stderr, "test_open: Must have a mode (r,w,rw) and file path.\n");
@@ -19,16 +19,21 @@ int main(int argc, char** argv) {
     file_path = argv[2];
 
     if (! strncmp("rw", mode_string, 2)) {
+        fprintf(stderr, "Debug: test_open: Setting mode: O_RDWR = %x.\n", O_RDWR);
         mode_flag = O_RDWR;
     } else if (! strncmp("r", mode_string, 1)) {
+        fprintf(stderr, "Debug: test_open: Setting mode: O_RDONLY = %x.\n", O_RDONLY);
         mode_flag = O_RDONLY;
     } else if (! strncmp("w", mode_string, 1)) {
+        fprintf(stderr, "Debug: test_open: Setting mode: O_WRONLY = %x.\n", O_WRONLY);
         mode_flag = O_WRONLY;
     } else {
         fprintf(stderr, "test_open: Unexpected mode ('%s').  Must be in (r,w,rw).\n", mode_string);
         exit(1);
     }
    
+    fprintf(stderr, "Debug: test_open: About to open \"%s\" with mode %x.\n", file_path, mode_flag);
+
     file_descriptor = open(file_path, mode_flag);
     if (file_descriptor == -1)
         return -errno;
